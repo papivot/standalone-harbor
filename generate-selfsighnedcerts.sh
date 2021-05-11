afcd41f75b98a55fd87b5efb7820f0d2ca22d1bc
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-05-11 16:14:56 -0400

    Create generate-selfsighnedcerts.sh

---
+++ generate-selfsighnedcerts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+fqdn=apps.navneetv.com
+#fqdn=apps.navlab.io
+
+# Generate a CA Cert Private Key"
+sudo openssl genrsa -out ca.key 4096
+
+# Generate a CA Cert Certificate"
+sudo openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=US/ST=VA/L=Ashburn/O=SE/OU=Personal/CN=${fqdn}" -key ca.key -out ca.crt
+
+# Generate a Server Certificate Private Key"
+sudo openssl genrsa -out ${fqdn}.key 4096
+
+# Generate a Server Certificate Signing Request"
+sudo openssl req -sha512 -new -subj "/C=US/ST=VA/L=Ashburn/O=SE/OU=Personal/CN=${fqdn}" -key ${fqdn}.key -out ${fqdn}.csr
+
+# Generate a x509 v3 extension file"
+cat > v3.ext <<-EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1=${fqdn}
+DNS.2=*.${fqdn}
+EOF
+
+# Use the x509 v3 extension file to gerneate a cert for the Harbor host"
+sudo openssl x509 -req -sha512 -days 3650 -extfile v3.ext -CA ca.crt -CAkey ca.key -CAcreateserial -in ${fqdn}.csr -out ${fqdn}.cert
