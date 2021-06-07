3900dd32375a04fc5cde2ab3e9eb010eff493247
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-06-07 13:55:46 -0400

    Update generate-selfsighnedcerts.sh

--- generate-selfsighnedcerts.sh
+++ generate-selfsighnedcerts.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-fqdn=apps.navneetv.com
-ipaddress=10.100.1.1
-#fqdn=apps.navlab.io
+# Modify these two variables
+fqdn=harbor.env1.lab.local
+ipaddress=192.168.100.1 
 
-# Generate a CA Cert Private Key"
-sudo openssl genrsa -out ca.key 4096
+sudo apt install -y ca-certificates
+cakeyfile=$(hostname)-ca.key
+cacrtfile=$(hostname)-ca.crt
 
-# Generate a CA Cert Certificate"
-sudo openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=US/ST=VA/L=Ashburn/O=SE/OU=Personal/CN=${fqdn}" -key ca.key -out ca.crt
+if [ ! -f "${cakeyfile}" ] || [ ! -f "${cacrtfile}" ]; then
+  echo "CA cert ${cacrtfile} and key ${cakeyfile} do not exist."
+	echo "Generating them before generating the server certificate..."
+
+	# Generate a CA Cert Private Key"
+	sudo openssl genrsa -out ${cakeyfile} 4096
+
+	# Generate a CA Cert Certificate"
+	sudo openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=US/ST=VA/L=Ashburn/O=SE/OU=Personal/CN=${fqdn}" -key ${cakeyfile} -out ${cacrtfile}
+
+	sudo cp -p ${cacrtfile} /usr/local/share/ca-certificates/${cacrtfile}
+	echo
+	echo "CA file ${cacrtfile} copied to /usr/local/share/ca-certificates/${cacrtfile}."
+	echo "Execute sudo update-ca-certificates after this script has completed execution"
+	echo
+	echo
+fi
 
 # Generate a Server Certificate Private Key"
 sudo openssl genrsa -out ${fqdn}.key 4096
@@ -31,4 +47,4 @@ IP.1=${ipaddress}
 EOF
 
 # Use the x509 v3 extension file to gerneate a cert for the Harbor host"
-sudo openssl x509 -req -sha512 -days 3650 -extfile v3.ext -CA ca.crt -CAkey ca.key -CAcreateserial -in ${fqdn}.csr -out ${fqdn}.cert
+sudo openssl x509 -req -sha512 -days 3650 -extfile v3.ext -CA ${cacrtfile} -CAkey ${cakeyfile} -CAcreateserial -in ${fqdn}.csr -out ${fqdn}.cert
